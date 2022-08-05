20c2e2fb587d26dbfa982c6c2fdb32ace6872a35
iToredo <denistorpeda1@gmail.com>
2022-08-05 18:41:06 +0300

    Add new task

---
+++ Objects/arrayObjectTransferInAnyArray.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        username: 'David',
+        status: 'online',
+        lastActivity: 10
+    },
+    {
+        username: 'Lucy',
+        status: 'offline',
+        lastActivity: 22
+    },
+    {
+        username: 'Bobe',
+        status: 'online',
+        lastActivity: 104
+    },
+];
+
+const usersOnline = users.filter((person) => person.status === 'online');
+const usersNameOnline = usersOnline.map((uNames) => uNames.username).join(',');
+    alert(`Сейчас в онлайне следующие пользователи: ${usersNameOnline}`);
+
