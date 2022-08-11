ea57573f8aec7781beadb9b3307ed7d0c639a016
iToredo <denistorpeda1@gmail.com>
2022-08-11 19:57:58 +0300

    Add new completed task.

---
+++ Objects/findKiller/getKiller.js
@@ -0,0 +1,21 @@
+function getKiller(suspectInfo, deadPeople) {
+let nameKiller = '';
+Object.entries(suspectInfo).forEach((data) => {
+    const suspectPersons = data[0];
+    const peopleWereSeen = data[1];
+    const findKiller = deadPeople.every((peopleDead) => peopleWereSeen.includes(peopleDead));
+        if (findKiller) {
+            nameKiller = suspectPersons;
+        }
+});
+    return console.log(nameKiller);
+}
+
+getKiller(
+    {
+        'Sena':['David', 'Kyle', 'Lucas'],
+        'Seda': ['Jacob', 'Bill', 'Lucas'],
+        'Jora': ['Lucy', 'Kyle'],
+    },
+    ['Lucas', 'Bill']
+);
